75ecf32822d747d2e3c45e83914c12f2ffede368
NReeve160 <reeve.nathan160@gmail.com>
2025-03-12 07:22:03 -0600

    Making sure this is up to date before work

--- languageProject/scripts.js
+++ languageProject/scripts.js
@@ -4,26 +4,58 @@ let question = {
     correctAnswer: 1
 };
 
+let questions = [
+    {
+        title: 'gato',
+        aleternatives: ['dog', 'cat', 'bird', 'fish'],
+        correctAnswer: 1
+    },
+    {
+        title: 'ave',
+        alternatives: ['mouse', 'hamster', 'lizard', 'bird'],
+        correctAnswer: 3
+    },
+    {
+        title: 'rata',
+        alternatives: ['cat', 'fish', 'rat', 'shark'],
+        correctAnswer: 2
+    },
+    {
+        title: 'mosca',
+        alternatives: ['fly', 'puma', 'fish', 'dog'],
+        correctAnswer: 0
+    }
+]
+
 let app = {
     start: function() {
+
+        this.currentPosition = 0;
+
         // get alternatives
         let alts = document.querySelectorAll('.alternative');
         console.log(alts);
+
+        console.log(this);
 
         // for each to update the alts
         alts.forEach(function(element, index) {
             // Selecting a item and adding a event listener
             element.addEventListener('click', function() {
                 // check correct answer
-                console.log('check correct answer');
-            });
-        });
+                this.checkAnswer(index);
+            }.bind(this));
+        }.bind(this));
 
         // show first question
-        this.showQuestion(question);
+        this.showQuestion();
     },
 
     showQuestion: function(q) {
+
+        //keep track of the current question
+        this.currQuestion = q;
+
         // select dom element
         let titleDiv = document.getElementById('title');
 
@@ -37,6 +69,28 @@ let app = {
             element.textContent = q.aleternatives[index];
         })
     },
+
+    checkAnswer: function(userSelected) {
+        if(this.currQuestion.correctAnswer == userSelected) {
+            //correct
+            console.log('Correct Answer');
+        } else
+        //incorrect
+        console.log('Incorrect Answer');
+
+        //increase position
+        this.increasePosition();
+        //show next question 
+        this.showQuestion(questions[this.currPosition]);
+    },
+
+    increasePosition: function() {
+        this.currPosition++;
+
+        if(this.currPosition == question.length) {
+            this.currPosition = 0;
+        }
+    }
 };
 
 app.start();
